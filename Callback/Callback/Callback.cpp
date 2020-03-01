1a8d412fe6d29db34c803ab89da6bf3bc0d9e656
Pollutedlake <psh950827@naver.com>
2020-03-02 03:18:56 +0900

    백그라운드 작업

---
+++ Callback/Callback/Callback.cpp
@@ -0,0 +1,73 @@
+﻿/*
+파일 : Callback.cpp
+작성자 : 박상현
+작성일 : 2020.03.02
+*/
+
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("Callback");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_VREDRAW | CS_HREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 500, 400, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+// hWnd : 타이머를 소유한 윈도우의 핸들
+// uMsg : WM_Timer
+// idEvent : 타이머 ID
+// dwTime : 윈도우즈가 실행된 후의 경과시간
+void CALLBACK TimerProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime) {
+	HDC hdc;
+	int i;
+	hdc = GetDC(hWnd);
+	for (i = 0; i < 1000; i++) {
+		SetPixel(hdc, rand() % 500, rand() % 400, RGB(rand() % 256, rand() % 256, rand() % 256));
+	}
+	ReleaseDC(hWnd, hdc);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+
+	switch (iMessage) {
+	case WM_CREATE:
+		SetTimer(hWnd, 1, 100, TimerProc);		// WM_TIMER 메시지를 보내는 대신 TimerProc 함수를 호출한다.
+		return 0;
+	case WM_LBUTTONDOWN:
+		hdc = GetDC(hWnd);
+		Ellipse(hdc, LOWORD(lParam) - 10, HIWORD(lParam) - 10, LOWORD(lParam) + 10, HIWORD(lParam) + 10);
+		ReleaseDC(hWnd, hdc);
+		return 0;
+	case WM_DESTROY:
+		KillTimer(hWnd, 1);
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
--- RandGrp/RandGrp/RandGrp.cpp
+++ RandGrp/RandGrp/RandGrp.cpp
@@ -1,5 +1,70 @@
 ﻿/*
-파일 :
+파일 : RandGrp.cpp
 작성자 : 박상현
-작성일 :
+작성일 : 2020.03.01
 */
+
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("RandGrp");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_VREDRAW | CS_HREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 500, 400, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	int i;
+
+	switch (iMessage) {
+	case WM_CREATE:
+		SetTimer(hWnd, 1, 50, NULL);	// 초당 20번 주기의 타이머
+		return 0;
+	case WM_TIMER:		// 한번 주기가 될 때마다 1000개의 점을 난수로 얻은 임의의 좌표에 임의 색상으로 출력한다.
+		hdc = GetDC(hWnd);
+		for (i = 0; i < 1000; i++) {
+			SetPixel(hdc, rand() % 500, rand() % 400, RGB(rand() % 356, rand() % 256, rand() % 256));
+		}
+		ReleaseDC(hWnd, hdc);
+		return 0;
+	case WM_LBUTTONDOWN:		// 백그라운드에 WM_TIMER 작업을 하면서도 다른 작업이 가능하다
+		hdc = GetDC(hWnd);
+		Ellipse(hdc, LOWORD(lParam) - 10, HIWORD(lParam) - 10, LOWORD(lParam) + 10, HIWORD(lParam) + 10);
+		ReleaseDC(hWnd, hdc);
+		return 0;
+	case WM_DESTROY:
+		KillTimer(hWnd, 1);		// 타이머 해제
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+// 이런 지속적으로 해야 할 작업은 타이머를 설치한 후 타이머 메시지를 받을 때마다 찔끔찔끔 나누어 해야 한다.
+// 잘게 나누어야 반응성이 좋아진다.

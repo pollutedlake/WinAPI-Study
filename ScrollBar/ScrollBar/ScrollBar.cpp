0d97ebbbf7583bc9c63bfc006cdd6f0800b9d9ad
Pollutedlake <psh950827@naver.com>
2020-06-01 17:58:00 +0900

    7-5 스크롤 바

---
+++ ScrollBar/ScrollBar/ScrollBar.cpp
@@ -0,0 +1,125 @@
+﻿#include<Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("Title");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return(int)Message.wParam;
+}
+
+// 다른 컨트롤들은 자신에게 변화가 있을 때 부모 윈도우로 WM_COMMAND 통지 메시지를 보내는데 비해 스크롤 바는 WM_HSCROLL(수평일 경우), WM_VSCROLL(수직일 경우)이라는 별도의 메시지를 부모 윈도우로 보낸다.
+// 이 메시지들은 단순한 사건 발생뿐만 아니라 스크롤 상태에 대한 추가 정보를 같이 보내야 하는데 WM_COMMAND의 파라미터는 이미 용도가 예약되어 있어 이 정보를 보낼 수 없다.
+
+#define ID_SCRRED 100
+#define ID_SCRGREEN 101
+#define ID_SCRBLUE 102
+HWND hRed, hGreen, hBlue;		// Red, Green, Blud의 스크롤 바 핸들
+int Red, Green, Blue;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+	HBRUSH MyBrush, OldBrush;
+	int TempPos;
+
+	switch (iMessage) {
+	case WM_CREATE:
+		// 스크롤 바는 "scrollbar"윈도우 클래스로 생성하며 수평 스크롤 바일 경우 SBS_HORZ 스타일을, 수직 스크롤 바일 경우는 SBS_VERT 스타일을 지정한다.
+		hRed = CreateWindow(TEXT("scrollbar"), NULL, WS_CHILD | WS_VISIBLE | SBS_HORZ, 10, 10, 400, 20, hWnd, (HMENU)ID_SCRRED, g_hInst, NULL);		// Red, Green, Blue의 스크롤 바 생성
+		hGreen = CreateWindow(TEXT("scrollbar"), NULL, WS_CHILD | WS_VISIBLE | SBS_HORZ, 10, 40, 400, 20, hWnd, (HMENU)ID_SCRGREEN, g_hInst, NULL);
+		hBlue = CreateWindow(TEXT("scrollbar"), NULL, WS_CHILD | WS_VISIBLE | SBS_HORZ, 10, 70, 400, 20, hWnd, (HMENU)ID_SCRBLUE, g_hInst, NULL);
+
+		// BOOL SetScrollRange(HWND hWnd, int nBar, int nMinPos, int nMaxPos, BOOL bRedraw); : 스크롤 바의 범위를 지정한다.
+		// hWnd : 스크롤 바의 윈도우 핸들
+		// nBarr : 메인 윈도우에 부착된 표준 스크롤 바 또는 별도의 스크롤 바 컨트롤을 지정하는데 이 값이 SB_CTL이면 별도의 컨트롤을 지정한다.
+		// nMinPos, nMaxPos : 최솟값, 최댓값을 지정한다.
+		SetScrollRange(hRed, SB_CTL, 0, 255, TRUE);
+
+		// int SetScrollPos(HWND hWnd, int nBar, int nPos, BOOL bReDraw); : 스크롤 바의 현재 위치값을 지정한다.
+		// nPos : 스크롤 바의 현재 위치
+		SetScrollPos(hRed, SB_CTL, 0, TRUE);
+		SetScrollRange(hGreen, SB_CTL, 0, 255, TRUE);
+		SetScrollPos(hGreen, SB_CTL, 0, TRUE);
+		SetScrollRange(hBlue, SB_CTL, 0, 255, TRUE);
+		SetScrollPos(hBlue, SB_CTL, 0, TRUE);
+		return 0;
+	case WM_HSCROLL:		// 스크롤 바에 변화가 생김.
+		if ((HWND)lParam == hRed) {		// lParam : 스크롤 바의 윈도우 핸들
+			TempPos = Red;
+		}
+		if ((HWND)lParam == hGreen) {
+			TempPos = Green;
+		}
+		if ((HWND)lParam == hBlue) {
+			TempPos = Blue;
+		}
+		switch (LOWORD(wParam)) {		// LOWORD(wParam) : 스크롤 바 내의 어디를 눌렀는가?
+		case SB_LINELEFT:		// 사용자가 왼쪽 화살표 버튼을 눌렀다는 뜻이며 이때는 왼쪽으로 한 단위 스크롤시킨다.
+			TempPos = max(0, TempPos - 1);
+			break;
+		case SB_LINERIGHT:		// 사용자가 오른쪽 화살표 버튼을 눌렀다는 뜻이며 이때는 오른쪽으로 한 단위 스크롤시킨다.
+			TempPos = min(255, TempPos + 1);
+			break;
+		case SB_PAGELEFT:		// 사용자가 왼쪽 몸통 부분을 눌렀다는 뜻이며 이떄는 한 페이지 왼쪽으로 스크롤시킨다.
+			TempPos = max(0, TempPos - 10);
+			break;
+		case SB_PAGERIGHT:		// 사용자가 오른쪽 몸통 부분을 눌렀다는 뜻이며 이떄는 한 페이지 오른쪽으로 스크롤시킨다.
+			TempPos = min(255, TempPos + 10);
+			break;
+		case SB_THUMBTRACK:		// 스크롤 박스를 드래그하고 있는 중이다. 이 메시지는 마우스 버튼을 놓을 때까지 계속 전달된다.
+			TempPos = HIWORD(wParam);		// HIWORD(wParam) : 현재 위치
+			break;
+		}
+		if ((HWND)lParam == hRed) {
+			Red = TempPos;
+		}
+		if ((HWND)lParam == hGreen) {
+			Green = TempPos;
+		}
+		if ((HWND)lParam == hBlue) {
+			Blue = TempPos;
+		}
+		SetScrollPos((HWND)lParam, SB_CTL, TempPos, TRUE);
+		InvalidateRect(hWnd, NULL, FALSE);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		MyBrush = CreateSolidBrush(RGB(Red, Green, Blue));
+		OldBrush = (HBRUSH)SelectObject(hdc, MyBrush);
+		Rectangle(hdc, 10, 100, 410, 300);
+		SelectObject(hdc, OldBrush);
+		DeleteObject(MyBrush);
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
---
+++ Static/Static/Static.cpp
@@ -0,0 +1,54 @@
+﻿#include<Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("Title");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return(int)Message.wParam;
+}
+
+// 스태틱은 사용자로부터 입력을 받아들이는 기능은 없고 오로지 문자열을 부여주는 것이 기능의 전부이다. 주로 에디트나 다른 컨트롤 옆에 위치하며 컨트롤의 용도를 설명하는 역할을 한다. 스태틱 컨트롤을 만들 때는 윈도우 클래스를 "static"으로 지정한다.
+// TexOut으로 출력한 문자열은 그냥 문자열일 뿐이므로 언제든지 지워질 수 있고 그래서 WM_PAINT에서 계속 복구해야 한다. 만면 스태틱 컨트롤은 스스로 메시지를 처리할 수 있는 윈도우이기 때문에 일단 배치해 놓기만 하면 더 이상 신경쓰지 않아도 된다.
+// 또한 색상, 글꼴 크기 등 운영체제의 세팅이 바뀔 경우 이런 변화에 대해서도 스스로 대처한다는 장점이 있다.
+// 컨트롤의 3요소 : 스타일, 통지 메시지, 부모 윈도우가 보내는 메시지
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	switch (iMessage) {
+	case WM_CREATE:
+		CreateWindow(TEXT("static"), TEXT("Only Text"), WS_CHILD | WS_VISIBLE, 20, 20, 100, 25, hWnd, (HMENU)-1, g_hInst, NULL);		// static 컨트롤은 실행중에 부모 윈도우로 통지 메시지를 보낼 필요가 없기 때문에 ID를 -1로 지정하며 여러 개의 스태틱 컨트롤이 있을 경우 모두 -1의 같은 ID를 사용해도 상관없다. ID -1은 ID를 주지 않는다는 뜻이며 더 이상 이 컨트롤을 프로그래밍할 필요가 없을 때 사용한다. 스태틱은 그 자리에 있는 자체가 존재의 의미이며 다른 컨트롤과 구분할 필요조차도 없기 때문이다.
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}

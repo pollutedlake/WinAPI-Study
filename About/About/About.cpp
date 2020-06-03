dbd609b4ef5f6e884903a35348a4294701345b17
Pollutedlake <psh950827@naver.com>
2020-06-03 16:25:23 +0900

    8-1 대화상자

---
+++ About/About/About.cpp
@@ -0,0 +1,81 @@
+﻿#include<Windows.h>
+#include"resource.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("About");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return(int)Message.wParam;
+}
+
+// 많은 양의 종보를 사용자로부터 효율적으로 입력받기 위해 주로 대화상자를 사용한다. 대화상자는 프로그램과 사용자간의 대화,곧 명령 및 정보 전달을 위한 특별한 윈도우이다. 대화상자에는 컨트롤들이 배치되기 때문에 컨트롤의 컨테이너라고도 한다.
+// 대화상자는 동작 방식에 따라 크게 모달형과 모델리스형으로 나뉘어진다. 모달형은 대화상자를 닫기 전에 다른 윈도우로 전환할 수 없으며 반드시 대화상자를 닫아야 다른 윈도우로 전환할 수 있다. 대화가 종료되어야 다른 일을 할 수 있거나 대화의 내용에 따라 다음 할 일이 결정되는 경우 모달 대화상자가 사용되며 대부분의 대화상자는 모달형이다.
+// 모델리스형은 대화상자를 열어 놓은 채로 다른 윈도우로 전환할 수 있는 대화상자이다. 프로그램의 상태를 나타내거나 작업을 하면서 참조해야 할 여러 가지 정보를 표시하는 윈도우가 모델리스형으로 만들어진다.
+// 대화상자를 만들기 위해서는 기본적으로 다음 두가지가 있어야 한다.
+// 대화상자 템플릿 : 대화상자의 모양과 대화상자 내의 컨트롤 배치 상태가 저장되는 이진 정보이며 리소스로 작성된다.
+// 대화상자 프로시저 : 대화상자에서 발생하는 메시지를 처리한다.
+
+BOOL CALLBACK AboutDlgProc(HWND hDlg, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	switch (iMessage) {
+		case WM_INITDIALOG:
+			return TRUE;
+		case WM_COMMAND:
+			switch (LOWORD(wParam)) {
+			case IDOK:
+				EndDialog(hDlg, IDOK);
+				return TRUE;
+			case IDCANCEL:
+				EndDialog(hDlg, IDCANCEL);
+				return TRUE;
+			}
+			break;
+	}
+	return FALSE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	switch (iMessage) {
+	case WM_LBUTTONDOWN:
+
+		// int DialogBox(HINSTANCE hInstance, LPCTSTR lpTemplate, HWND hWndParent, DLGPROC lpDialogFunc); : 대화상자를 호출하는 함수이며 대화상자의 출력, 운영 및 종료까지 대화상자의 모든 처리를 다 책임진다.
+		// hInstance : 대화상자 리소스를 가진 인스턴스의 핸들
+		// lpTemplate : 대화상자 템플릿의 리소스 ID
+		// hWndParent : 대화상자를 소유할 부모 윈도우
+		// lpDialogFunc : 대화상자 프로시저의 이름
+		// DialogBox는 hInstance의 리소스 중 lpTemplate가 지정하는 템플릿을 읽어와 대화상자를 만들어 화면에 출력한다. 이 때 템플릿에 기록된 차일드 컨트롤의 정보를 참조하여 포함된 컨트롤도 생성하여 배치한다.
+		// 그리고 lpDialogFunc 함수로 대화상자 운용에 필요한 메시지를 보내 대화상자가 제데로 실행될 수 있도록 한다. lpDialogFunc 함수에서는 대화상자와 그 차일드들 간의 통신을 처리할 것이다.
+		// 사용자가 대화상자를 닫으면 DialogBox 함수가 리턴하는데 이때 리턴되는 값은 대화상자의 종료 함수인 EndDialog 함수가 지정하는 값이다.
+		DialogBox(g_hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, AboutDlgProc);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
---
+++ DlgBase/DlgBase/DlgBase.cpp
@@ -0,0 +1,40 @@
+﻿// 대화상자 프로시저와 윈도우 프로시저는 비슷한 역할을 하며 모양도 비슷하지만 차이점도 있다.
+// 첫 번째로 두 프로시저의 리턴값이 다른데 윈도우 프로시저는 LRESULT(long)형의 값을 리턴하지만 대화상자 프로시저는 BOOL형의 값을 리턴한다.
+// 윈도우 프로시저는 메시지를 처리했을 때 0을 리턴하고 관심없는 메시지는 DefWindowProc으로 보내지만 대화상자 프로시저는 메시지를 처리했으면 TRUE를 리턴하고 처리하지 못했으면 FALSE를 리턴한다.
+// 만약 대화상자 프로시저가 메시지를 처리하지 못해 FALSE를 리턴했다면 그 메시지에 대한 나머지 처리는 운영체제가 알아서 디폴트 처리한다. 대화상자 프로시저는 관심없는 메시지에 대해 DefWindowProc을 호출할 필요가 없으며 대신 FALSE를 리턴하면 된다. 그래서 대화상자 프로시저의 끝은 항상 return FALSE이다.
+// 두 번째로 대화상자 프로시저는 WM_CREATE 메시지 대신 WM_INITDIALOG 메시지를 받아들이며 이 메시지를 받은 시점에서 대화상자에 필요한 초기화를 하면 된다.
+// 대화상자 프로시저에서 주로 처리하는 메시지는 WM_CREATE이다. 이 메시지는 대화상자에서 컨트롤이나 메뉴항목이 선택될 경우 컨트롤들이 부모 윈도우인 대화상자로 보내는 통지 메시지이다.
+// 일반 윈도우에서와 마찬가지로 LOWORD(wParam)에 메시지를 보낸 컨트롤의 ID가 전달되며 HIWORD(wParam)에 통지 코드가 전달된다.
+
+// 대화상자가 메인 윈도우가 되는 형태의 프로그램을 대화상자 기반의 프로그램(Dialog Base)이라고 한다.
+
+#include<Windows.h>
+#include "Resource.h"
+
+BOOL CALLBACK MainDlgProc(HWND hDlg, UINT iMessage, WPARAM wParam, LPARAM lParam);
+HWND hDlgMain;
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), HWND_DESKTOP, MainDlgProc);		// 대화상자의 부모는 HWND_DESKTOP으로 지정하여(NULL로 지정하는 것과 동일하다) 이 대화상자가 메인 윈도우가 되도록 했다.
+	return 0;
+}
+
+BOOL CALLBACK MainDlgProc(HWND hDlg, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	switch (iMessage) {
+	case WM_INITDIALOG:
+		hDlgMain = hDlg;
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam)) {
+		case IDOK:
+		case IDCANCEL:		// IDCANCEL은 Cancel버튼의 ID임과 동시에 대화상자의 닫기 버튼(X버튼)의 ID이기도 하므로 이 버튼을 제거했다 하더라도 반드시 IDCANCEL에 대한 통지 메시지는 처리해야 한다. 그렇지 않으면 대화상자가 닫히지 않는다.
+
+			// BOOL EndDialog(HWND hDlg, int nResult); : 이 함수가 호출되면 모달 대화상자가 닫힌다.
+			// nResult : 대화상자를 호출한 DialogBox 함수의 리턴값으로 전달되는데 주로 취소 여부를 리턴한다. 대개의 경우 DialogBox를 호출한 곳에서는 이 함수의 리턴값을 보고 사용자가 대화상자에서 어떤 버튼을 눌렀는지 판별하고 다음 동작을 결정한다.
+			EndDialog(hDlg, IDOK);
+			return TRUE;
+		}
+		return FALSE;
+	}
+	return FALSE;
+}
